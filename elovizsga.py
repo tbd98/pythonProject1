8dceb22208e827a346362489fe8c5f1af5a36a70
Bence Dániel Tóth <tothbence55@gmail.com>
2023-11-30 17:34:13 +0100

    foglalasok kezelese commit

--- elovizsga.py
+++ elovizsga.py
@@ -1,4 +1,4 @@
-from abc import ABC
+from abc import ABC, abstractclassmethod
 
 
 class Szoba(ABC):
@@ -24,6 +24,33 @@ class Szalloda:
     def __init__(self, nev):
         self.nev = nev
         self.szobak = []
+        self.foglalasok = []
+
+        def add_szoba(self, szoba):
+            self.szobak.append(szoba)
+
+        def add_foglalas(self, foglalas):
+            self.foglalasok.append(foglalas)
+
+        def foglalas(self, szobaszam, datum):
+            for szoba in self.szobak:
+                if szoba.szobaszam == szobaszam:
+                    foglalas = Foglalas(szoba=szoba, datum=datum)
+                    self.add_foglalas(foglalas)
+                    return szoba.ar
+            return None
+
+        def lemondas(self, szobaszam, datum):
+            for foglalas in self.foglalasok:
+                if foglalas.szoba.szobaszam == szobaszam and foglalas.datum == datum:
+                    self.foglalasok.remove(foglalas)
+                    return "lemondva"
+                return "nincs ilyen foglalas"
+
+        def osszes_foglalas(self):
+            print(f"a(z){self.nev} összes foglalása:")
+            for foglalas in self.foglalasok:
+                print(f"Foglalás - Szoba: {foglalas.szoba.szobaszam}, Dátum: {foglalas.datum}")
 
 
 class Foglalas:
@@ -32,4 +59,4 @@ class Foglalas:
         self.datum = datum
 
 
-print('vege')
+print("Tóth Bence Dániel, RU1BB4, mérnökinformatikus BSc.")
